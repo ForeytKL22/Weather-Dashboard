225627d8ddc131fa80144580d21c0ea6fc35d0f6
ForeytKL22 <krforeyt@gmail.com>
2021-09-07 22:08:50 -0600

    got today's forecast appended

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
-// GIVEN a weather dashboard with form inputs
-// WHEN I search for a city
+
 var searchFormEl = document.querySelector("#search-form");
 var cityName = document.querySelector("#search-city");
+
+
 var userSubmitBtn = document.querySelector("#search-btn");
 var searchedCityName = document.querySelector("#searched-city-name");
 
-//when you click submit button to search city
+var cityTemp = document.querySelector("#city-temp");
+var cityWind = document.querySelector("city-wind");
+
+var pWind = document.createElement("p");
+var pHumidity = document.createElement("p");
+var pUv = document.createElement("p");
+
+
 
 
 
@@ -27,7 +35,7 @@ var clickSubmitEl = function(event) {
 
 
 var getWeatherForecast = function(search) {
-    var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + search + "&appid=d3f5af43f561d831f34569cf6fef321f";
+    var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + search + "&units=imperial&appid=d3f5af43f561d831f34569cf6fef321f";
 
     fetch(apiUrl)
         .then(function(response) {
@@ -36,53 +44,61 @@ var getWeatherForecast = function(search) {
                 response.json().then(function(data) {
                     console.log(data);
                     displaySearch(data, search)
+                    getWeatherUvi(data.coord);
                 })
             } 
         });
-
 };
 
+
+var getWeatherUvi = function(data) {
+    var {lat} = data;
+    var {lon} = data;
+    console.log({lat});
+    var apiUrl2 = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&appid=d3f5af43f561d831f34569cf6fef321f";
+    console.log(data);
+    fetch(apiUrl2)
+        .then(function(response) {
+            if (response.ok) {
+                console.log(response);
+                response.json().then(function(data) {
+                    console.log(data);
+                    displaySearch2(data);
+                    
+                })
+            }
+        })
+    
+}
 
 
 var displaySearch = function(weatherData, searchedCity) {
     searchedCityName.textContent = searchedCity;
+
+    cityTemp.textContent = "Temp: " + weatherData.main.temp;
+
+    
+    pWind.textContent = "Wind speed: " + weatherData.wind.speed + " MPH";
+    cityTemp.appendChild(pWind);
+
+   
+    pHumidity.textContent = weatherData.main.humidity;
+    pWind.appendChild(pHumidity);
+    
+};
+
+
+var displaySearch2 = function(data) {
+
+    pUv.textContent = data.current.uvi;
+    pWind.appendChild(pUv);
+    console.log(data.current.uvi);
+
 }
 
 
-// searchedCityName.textContent = searchedCity;
 
 
-// api.openweathermap.org/data/2.5/weather?q={city name}&appid=d3f5af43f561d831f34569cf6fef321f
-
-// THEN I am presented with current and future conditions for that city and that city is added to the search history
-
-
-
-
-
-// WHEN I view current weather conditions for that city
-// THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index
-
-
-
-
-
-// WHEN I view the UV index
-// THEN I am presented with a color that indicates whether the conditions are favorable, moderate, or severe
-
-
-
-
-
-// WHEN I view future weather conditions for that city
-// THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
-
-
-
-
-
-// WHEN I click on a city in the search history
-// THEN I am again presented with current and future conditions for that city
 
 
 searchFormEl.addEventListener("submit", clickSubmitEl);
